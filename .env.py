1448664efd8b2c2c86ee255a17572b676822e451
TheDKnight <113733655@umail.ucc.ie>
2023-05-04 21:13:25 +0000

    added key

--- .env.py
+++ .env.py
@@ -1,3 +1,4 @@
 DJANGO_SECRET_KEY=django-insecure-@9g9-&d9!zdo(23+8v5)as8w#mbt6-#8+#%1dtr(=_*(++++t!
+DATABASE_URL = postgres://jchoenfo:iLNXMqAKKizg6PctrzFBT8zTcxofEvVh@horton.db.elephantsql.com/jchoenfo
 STRIPE_PUBLIC_KEY = pk_test_51Myir9IAah2bBjJV7vuzDpnWnsOVPOOX9SntxZjUlrgQtfexgc2KemGHtLXOzwnmPshjqo140dP940fq0iDIJGyM00ulsf301R
 STRIPE_SECRET_KEY = sk_test_51Myir9IAah2bBjJVeB7PvWHwFkng96Ip4jUc3l9jifP4vMNDffZGZZpEgnQNkIJdFpnJGLVIM8MkitxDgwf0JsBa00hUlolst9
