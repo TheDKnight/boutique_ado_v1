4b4a6649a9acf6c7766e22c359b7f674cecd2962
TheDKnight <113733655@umail.ucc.ie>
2023-05-04 21:05:36 +0000

    heroku deployment

--- .env.py
+++ .env.py
@@ -1,3 +1,3 @@
-DJANGO_SECRET_KEY= django-insecure-@9g9-&d9!zdo(23+8v5)as8w#mbt6-#8+#%1dtr(=_*(++++t!
+DJANGO_SECRET_KEY=django-insecure-@9g9-&d9!zdo(23+8v5)as8w#mbt6-#8+#%1dtr(=_*(++++t!
 STRIPE_PUBLIC_KEY = pk_test_51Myir9IAah2bBjJV7vuzDpnWnsOVPOOX9SntxZjUlrgQtfexgc2KemGHtLXOzwnmPshjqo140dP940fq0iDIJGyM00ulsf301R
 STRIPE_SECRET_KEY = sk_test_51Myir9IAah2bBjJVeB7PvWHwFkng96Ip4jUc3l9jifP4vMNDffZGZZpEgnQNkIJdFpnJGLVIM8MkitxDgwf0JsBa00hUlolst9
