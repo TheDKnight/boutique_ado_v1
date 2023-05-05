22cfc03292f692179a55708ae70d54ee0dc55ce8
TheDKnight <113733655@umail.ucc.ie>
2023-05-05 20:48:07 +0000

    enabled debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-thedknight-boutiqueadov-klr3bosbe2z.ws-eu96b.gitpod.io', 'ecom-edu.herokuapp.com', 'localhost']
 
